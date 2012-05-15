b54c448f11860f37f725611878372495918324eb
girinoboy <girinoboy@d9cea0ff-1a89-7d96-0276-9d4b46cf5370>
2012-05-15 20:37:31 +0000

    testar funcionalidades de hibernate/prime-face

---
+++ trunk/SisGemat/src/br/com/config/HibernateUtil.java
@@ -0,0 +1,65 @@
+package br.com.config;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+import br.com.tables.Event;
+import br.com.tables.MockObject;
+
+public class HibernateUtil {
+
+   // private static final SessionFactory sessionFactory = buildSessionFactory();
+    
+    private static final SessionFactory sessionFactory;
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    
+    static
+    {
+        try
+        {
+            AnnotationConfiguration config = new AnnotationConfiguration();
+            config.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+            config.setProperty("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
+            //config.setProperty("hibernate.connection.url", "jdbc:hsqldb:mem:.demodb");
+            config.setProperty("hibernate.connection.url", "jdbc:hsqldb:file:./data/contratos;shutdown=true");
+            config.setProperty("hibernate.connection.username", "sa");
+            config.setProperty("hibernate.connection.password", "");
+           //config.setProperty("hibernate.default_schema", "prontscm");
+            config.setProperty("hibernate.connection.pool_size", "1");
+            config.setProperty("hibernate.connection.autocommit", "true");
+            config.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.NoCacheProvider");
+            config.setProperty("hibernate.hbm2ddl.auto", "update");
+            config.setProperty("hibernate.show_sql", "true");
+            config.setProperty("hibernate.transaction.factory_class", "org.hibernate.transaction.JDBCTransactionFactory");
+            config.setProperty("hibernate.current_session_context_class", "thread");
+ 
+            // Add your mapped classes here:
+            config.addAnnotatedClass(Event.class);
+            config.addAnnotatedClass(MockObject.class);
+ 
+            sessionFactory = config.buildSessionFactory();
+        }
+        catch (Throwable ex)
+        {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
