490b54d02a3ad9f326a64f13952c6a49913244ac
girinoboy <girinoboy@d9cea0ff-1a89-7d96-0276-9d4b46cf5370>
2012-07-02 04:47:15 +0000

    cadastro, falta criar pagina

--- trunk/SisGemat/src/br/com/cadastro/CadastroUsuarioAction.java
+++ trunk/SisGemat/src/br/com/cadastro/CadastroUsuarioAction.java
@@ -2,6 +2,7 @@ package br.com.cadastro;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
 
@@ -11,10 +12,14 @@ import br.com.config.Conexao;
 import br.com.config.HibernateUtil;
 import br.com.tables.Usuario;
 
-@ManagedBean(name = "CadastroUsuarioAction")
+@ManagedBean(name = "cadastroUsuarioAction")
 public class CadastroUsuarioAction {
 
-	private Usuario usuario;
+	private Usuario usuarioDTO;
+	
+	public CadastroUsuarioAction(){
+		usuarioDTO = new Usuario();
+	}
 
 	public String load(){
 		return "load.fwd";
@@ -25,23 +30,34 @@ public class CadastroUsuarioAction {
 
 			Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 			session.beginTransaction();
-			session.save(usuario);
+			if(usuarioDTO.getAtivoInativo() == null)
+				usuarioDTO.setAtivoInativo(true);
+			if(usuarioDTO.getDataNascimento() == null)
+				usuarioDTO.setDataNascimento(new Date());
+			if(usuarioDTO.getCpf()==null)
+				usuarioDTO.setCpf("00000000000");
+			session.save(usuarioDTO);
 
 			session.getTransaction().commit();
 
 		}catch(Exception e){
 			e.printStackTrace();
+			return "erroCadastrar.fwd";
 		}
 
 		return "cadastrar.fwd";
 	}
 
-	public Usuario getUsuario() {
-		return usuario;
+	public Usuario getUsuarioDTO() {
+		if(usuarioDTO == null){
+			usuarioDTO = new Usuario();
+		}
+		return usuarioDTO;
 	}
 
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
+	public void setUsuarioDTO(Usuario usuarioDTO) {
+		this.usuarioDTO = usuarioDTO;
 	}
+
 
 }
