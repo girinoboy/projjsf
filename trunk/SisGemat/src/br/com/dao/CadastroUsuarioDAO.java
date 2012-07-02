ab7c6924bb1854531176ca09f128ce4cb7550cd4
girinoboy <girinoboy@d9cea0ff-1a89-7d96-0276-9d4b46cf5370>
2012-07-02 04:23:01 +0000

    login com banco de dados e navegação, é so cadastrar o usuario que deseja logar na mão, depois vou implementar um cadastro

---
+++ trunk/SisGemat/src/br/com/dao/CadastroUsuarioDAO.java
@@ -0,0 +1,9 @@
+package br.com.dao;
+
+public class CadastroUsuarioDAO {
+
+	public CadastroUsuarioDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- trunk/SisGemat/src/br/com/managedBean/LoginBean.java
+++ trunk/SisGemat/src/br/com/managedBean/LoginBean.java
@@ -1,10 +1,18 @@
 package br.com.managedBean;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Date;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
+import br.com.cadastro.CadastroUsuarioAction;
 import br.com.config.HibernateUtil;
 import br.com.dao.LoginDAO;
 import br.com.tables.Usuario;
@@ -15,21 +23,10 @@ public class LoginBean {
 
 	private String	usuario;
 	private String	senha;
-
-	public String getUsuario() {
-		return this.usuario;
-	}
-
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
-
-	public String getSenha() {
-		return this.senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
+	private Usuario usuarioDTO;
+	
+	public LoginBean(){
+		
 	}
 
 	public String verificaLogin() {
@@ -46,22 +43,76 @@ public class LoginBean {
 
 	public String logar() {
 		
-		try{
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		try{
+		
 		session.beginTransaction();
 		
-		Usuario amigo = (Usuario)session.load(Usuario.class,1l);
-		System.out.println(amigo.getLogin());
+		//Usuario amigo = (Usuario)session.load(Usuario.class,1l);
+		//System.out.println(amigo.getLogin());
 		//session.getTransaction().commit();
 		
-		LoginDAO dao = new LoginDAO();   
-	    dao.retrieve(1L);  
-	    dao.getLogin(usuario);
+		//LoginDAO dao = new LoginDAO();   
+		// dao.retrieve(1L);  
+		// dao.getLogin(usuarioDTO);
+		/*
+		sCadastroUsuarioAction cadastro = new CadastroUsuarioAction();
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setLogin("marcleonio");
+    	usuario.setSenha("123456");
+    	usuario.setAtivoInativo(true);
+    	usuario.setDataNascimento(new Date());
+    	usuario.setCpf("11111111111");
+		cadastro.setUsuario(usuario);
+		
+    	cadastro.cadastrar();*/
+		
+        Criteria b = session.createCriteria(Usuario.class);
+        b.add( Restrictions.eq( "login",  usuarioDTO.getLogin()) );
+        b.add( Restrictions.eq( "senha",  usuarioDTO.getSenha()) );
+        List<Usuario> objetos = b.list(); 
+        if(objetos.size()>0){
+        	Usuario u = objetos.get(0);
+        	System.out.println(u.getLogin());
+        }else{
+        	throw new Exception();
+        }
 		}catch(Exception e){
 			e.printStackTrace();
+			return "fail";
+		}finally{
+			session.close();  
 		}
 		
 		return "logar";
+	}
+	
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Usuario getUsuarioDTO() {
+		if(usuarioDTO == null){
+			usuarioDTO = new Usuario();
+		}
+		return usuarioDTO;
+	}
+
+	public void setUsuarioDTO(Usuario usuarioDTO) {
+		this.usuarioDTO = usuarioDTO;
 	}
 
 }
